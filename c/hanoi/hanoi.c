3cb36704c06edb91e1303597cea02b4ff5b1293a
zzcnt <zzcontinent@163.com>
2024-06-15 23:18:57 +0800

    [add] hanoi

---
+++ c/hanoi/hanoi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//#define NEED_PRINT
+
+// C recursive function to solve tower of hanoi puzzle
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+{
+	if (n == 1)
+	{
+#ifdef NEED_PRINT
+		printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod);
+#endif
+		return;
+	}
+	towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+#ifdef NEED_PRINT
+	printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod);
+#endif
+	towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+}
+
+int main()
+{
+	int n = 32; // Number of disks
+	towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods
+	return 0;
+}
+
